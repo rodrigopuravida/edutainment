eac3f4bcfcf3c86544fc987cf7f7621cfe3544f0
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-21 19:15:10 -0700

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Rodrigo Carballo on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
